98de4e7486dc65a3b0083f5c93da3dcd2d737d56
Ahmed Abououf <aabuauf@yahoo.com>
2019-08-23 17:06:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-get '/reversename/:name' do
-  params[:name].reverse
-end
+  get '/reversename/:name' do
+    params[:name].reverse
+  end
 
+  get '/reversename/:name' do
+    params[:name].reverse
+  end
 end
