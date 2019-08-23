a22806175a82f4ad3cf4f8e775974c23528f298c
Ahmed Abououf <aabuauf@yahoo.com>
2019-08-23 17:26:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
   get "/say/:number/:phrase" do
     i = 0
     while i< params[:number].to_i do
-     Write "#{params[:phrase]}"
+      "#{params[:phrase]}"
      i+=1
     end
   end
