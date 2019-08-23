86aa4d4b01e18e281708fd3bd89a5e8a55845507
Ahmed Abououf <aabuauf@yahoo.com>
2019-08-23 17:49:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -30,7 +30,7 @@ class App < Sinatra::Base
       "#{params[:number1].to_i + params[:number2].to_i}"
     elsif  params[:operation] == "subtract" 
       "#{params[:number1].to_i - params[:number2].to_i}"
-    elsif  params[:operation] == "multiply " 
+    elsif  params[:operation] == "multiply" 
       "#{params[:number1].to_i * params[:number2].to_i}"
     elsif  params[:operation] == "divide" 
       "#{params[:number1].to_i / params[:number2].to_i}"
