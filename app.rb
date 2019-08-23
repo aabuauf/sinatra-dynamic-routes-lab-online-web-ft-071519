d24c8367c094522f70c42037d4021eee07a387b6
Ahmed Abououf <aabuauf@yahoo.com>
2019-08-23 17:00:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+get '/medicines/:id' do
+  @medicine = all_the_medicines.select do |medicine|
+    medicine.id == params[:id]
+  end.first
+  erb :'/medicines/show.html'
+end
 
 end
